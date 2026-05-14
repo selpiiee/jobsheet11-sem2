4a9fa6894e9aa023c69e90d8b000af058afc35ae
“selpiiee” <selvynurkhoffifa@gmail.com>
2026-05-14 13:39:50 +0700

    Experiment 1 -Node 21

---
+++ Node21.java
@@ -0,0 +1,13 @@
+public class Node21 {
+    Student21 data;
+    Node21 next;
+
+    public Node21() {
+
+    }
+
+    public Node21(Student21 data, Node21 next) {
+        this.data = data;
+        this.next = next;
+    }
+}
