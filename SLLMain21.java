fcdafc612b49103498d50db6c4aec99be64746a5
“selpiiee” <selvynurkhoffifa@gmail.com>
2026-05-14 13:49:37 +0700

    Experiment 1 - Modify SLL Main 21

--- SLLMain21.java
+++ SLLMain21.java
@@ -1,20 +1,35 @@
+import java.util.Scanner;
+
 public class SLLMain21 {
     public static void main(String[] args) {
         SingleLinkedList21 sll = new SingleLinkedList21();
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("--- Student Data Input (Linked List) ---");
+        
+        for (int i = 0; i < 3; i++) {
+            System.out.println("\nEnter data for Student " + (i + 1));
+            
+            System.out.print("NIM   : ");
+            String nim = sc.nextLine();
+            
+            System.out.print("Name  : ");
+            String name = sc.nextLine();
+            
+            System.out.print("Class : ");
+            String className = sc.nextLine();
+            
+            System.out.print("GPA   : ");
+            double gpa = sc.nextDouble();
+            sc.nextLine();
 
-        Student21 std1 = new Student21("001", "Student 1", "TI-1I", 3.89);
-        Student21 std2 = new Student21("002", "Student 2", "TI-1I", 3.45);
-        Student21 std3 = new Student21("003", "Student 3", "TI-1I", 3.20);
-        Student21 std4 = new Student21("004", "Student 4", "TI-1I", 3.00);
+            Student21 std = new Student21(nim, name, className, gpa);
+            sll.addLast(std);
+        }
 
+        System.out.println("\n--- Final Student List ---");
         sll.print();
-        sll.addFirst(std4);
-        sll.print();
-        sll.addLast(std1);
-        sll.print();
-        sll.insertAfter(std3, "Student 4");
-        sll.insertAt(2, std2);
-        sll.print();
+        
+        sc.close();
     }
-    
 }
